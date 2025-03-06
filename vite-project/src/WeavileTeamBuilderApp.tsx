71fa60af17989e4f32bf134e6cc5cbf7d424d166
Miguel Rodríguez <jazmindemar33@gmail.com>
2025-03-06 19:00:33 +0100

    Primer commit

---
+++ vite-project/src/WeavileTeamBuilderApp.tsx
@@ -0,0 +1,9 @@
+import { AppRouter } from './router/AppRouter.tsx'
+
+export const WeavileTeamBuilderApp = () => {
+  return (
+    <>
+        <AppRouter/>
+    </>
+  )
+}
---
+++ vite-project/src/router/AppRouter.tsx
@@ -0,0 +1,13 @@
+import { Routes } from "react-router";
+import { Route } from "react-router";
+import { MainPage } from "../sections/main/pages/MainPage";
+
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+    </Routes>
+  );
+};
+
