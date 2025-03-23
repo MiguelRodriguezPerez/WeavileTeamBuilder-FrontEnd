33610a2c1698b4d222b41d9e65f7dcced484f792
Miguel Rodríguez <jazmindemar33@gmail.com>
2025-03-23 10:16:15 +0100

    Zustand context persisted

--- src/globalContext/WeavileStore.tsx
+++ src/globalContext/WeavileStore.tsx
@@ -1,25 +1,31 @@
 import { create } from "zustand";
 import { WeavileState } from "./WeavileState";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 /* Casi todo en ts requiere un tipo, en este caso, una interface */
 
-const useWeavileStore = create<WeavileState>((set) => ({
-    selectedPokemonTeam: null,
-    changeSelectedTeam: (team) => set({ selectedPokemonTeam: team }),
+const useWeavileStore = create(persist<WeavileState>(
+    (set) => ({
+        selectedPokemonTeam: null,
+        changeSelectedTeam: (team) => set({ selectedPokemonTeam: team }),
 
-    /* DISCLAIMER: Esto fue una idea de chatgpt y estoy convencido de que existe una manera más limpia
-    de hacerlo */
-    changeSelectedTeamName: (newName: string) =>
-        set((state) => ({
-            selectedPokemonTeam: state.selectedPokemonTeam
-                ? { ...state.selectedPokemonTeam, name: newName }
-                : null,
-    })),
+        /* DISCLAIMER: Esto fue una idea de chatgpt y estoy convencido de que existe una manera más limpia
+        de hacerlo */
+        changeSelectedTeamName: (newName: string) =>
+            set((state) => ({
+                selectedPokemonTeam: state.selectedPokemonTeam
+                    ? { ...state.selectedPokemonTeam, name: newName }
+                    : null,
+        })),
+        deleteSelectedTeam: () => set({ selectedPokemonTeam: null }),
 
-    deleteSelectedTeam: () => set({ selectedPokemonTeam: null }),
-
-    selectedPokemonMember: null,
-    changeSelectedPokemon: (pokemon) => set({ selectedPokemonMember: pokemon }),
-}));
+        selectedPokemonMember: null,
+        changeSelectedPokemon: (pokemon) => set({ selectedPokemonMember: pokemon }),
+    }),
+    {
+        name: 'weavile-state',
+        storage: createJSONStorage(() => localStorage), // (opcional) valor por defecto 'localStorage'
+    },   
+));
 
 export default useWeavileStore;
