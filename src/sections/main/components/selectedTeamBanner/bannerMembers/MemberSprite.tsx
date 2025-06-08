9a340b802d8afb3703cd4538d1b92b65d5e6f174
Miguel Rodríguez <jazmindemar33@gmail.com>
2025-06-08 20:07:14 +0200

    SelectedTeamBanner responsive

--- src/sections/main/components/selectedTeamBanner/bannerMembers/MemberSprite.tsx
+++ src/sections/main/components/selectedTeamBanner/bannerMembers/MemberSprite.tsx
@@ -3,32 +3,26 @@ import { PokemonTeamMember } from "../../../../../domain/teamMemberEntities";
 import useWeavileStore from "../../../../../globalContext/WeavileStore";
 
 import styles from '../../../styles/selectedTeamBanner/selectedMember/bannerMemberSprite.module.css'
+import { useChooseSpriteToRender } from "../../../hooks/selectedTeamBanner/useChooseSpriteToRender";
+import { useMemo } from "react";
 
 export const MemberSprite = ({ member } : { member: PokemonTeamMember }) => {
 
     const selectedMember = useWeavileStore(state => state.selectedPokemonMember)!;
     const { width } = useWindowSize();
-
-    /* Para mostrar el sprite la debes convertir en una url temporal
-    que pueda usar el atributo src de img */
-    
+    const { chooseSpriteToRender } = useChooseSpriteToRender(member);
+    const spriteToRender: string = useMemo(() => chooseSpriteToRender(),[selectedMember, width]);
 
     return (
         <div className={styles['selected-member-sprite']}>
             <img 
-                src={
-                    member.name !== null ? 
-                        width > 700 ? `data:image/jpeg;base64,${ member.front_default_sprite }` : `data:image/jpeg;base64,${ member.pc_sprite }`
-                        :
-                        '/images/main/missignoTeamBanner.png'
-                } 
-                className={`${styles['heading-pokemon-sprite']} ${member.id === selectedMember.id ? styles['selected-pokemon-sprite'] : ''}`}
-            alt= {member ? member.name + '.png' : 'missigno.png'} />
+                src={ spriteToRender } 
+                className={`${styles['heading-pokemon-sprite']} 
+                    ${ member.id === selectedMember.id ? styles['selected-member'] : ''}`}
+                /* TODO: Encontrar una solución seria */
+                style={ spriteToRender.includes('missignoPhone') ? { paddingBottom: '0px' } : undefined }
 
-            {/* <img
-                src={sprite ? `data:image/jpeg;base64,${sprite}` : "/images/main/missignoTeamBanner.png"}
-                className={`heading-pokemon-sprite ${memberId === selectedMember.id ? 'member-selected' : ''}`}
-            /> */}
+            alt= {member ? member.name + '.png' : 'missigno.png'} />
         </div>
     );
 
