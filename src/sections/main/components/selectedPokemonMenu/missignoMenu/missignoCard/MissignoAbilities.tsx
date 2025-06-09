8657c02753ce26da106204ec875638c1fc082b27
Miguel Rodríguez <jazmindemar33@gmail.com>
2025-06-09 17:41:53 +0200

    MissignoGrid responsive maaaal

--- src/sections/main/components/selectedPokemonMenu/missignoMenu/missignoCard/MissignoAbilities.tsx
+++ src/sections/main/components/selectedPokemonMenu/missignoMenu/missignoCard/MissignoAbilities.tsx
@@ -1,14 +1,20 @@
 import { AbilityData } from "../../../../../../domain/dataEntities";
 import { parseAbility } from "../../../../../../globalHelpers/parseAbility";
 
-import '../../../../styles/missignoMenu/missignoCard.css'
+import styles from '../../../../styles/missignoMenu/missignoCard.module.css';
 
 export const MissignoAbilities = ({ abilityList } : { abilityList : AbilityData[] }) => {
     return (
-        <span className="missigno-abilities-list">
+        <span className={styles['missigno-abilities-list']}>
             {
                 abilityList.map((ability, index) => (
-                    <span key={index}>{parseAbility(ability.name)}</span>
+                    <span key={index}
+                        /* Las habilidades en showdown van de la siguiente manera. 
+                        Si hay dos, es un grid normal, pero si hay tres, será un grid de dos columnas
+                        y la habilidad que no comparte columna con otra se anexiona el siguiente row vacío */
+                        style={
+                            (abilityList.length === 3 && index === 1) ? { gridRow: '1/3' } : undefined
+                        }>{parseAbility(ability.name)}</span>
                 )) 
             }
         </span>
--- src/sections/main/components/selectedPokemonMenu/missignoMenu/missignoCard/MissignoSprite.tsx
+++ src/sections/main/components/selectedPokemonMenu/missignoMenu/missignoCard/MissignoSprite.tsx
@@ -1,12 +1,16 @@
 import { MissignoDTO } from "../MissignoDTO";
 
+import styles from '../../../../styles/missignoMenu/missignoCard.module.css';
+
 export const MissignoSprite = ({ dto } : { dto: MissignoDTO }) => {
   
     return (
-        <img 
-            src={  dto.pc_sprite ? `data:image/png;base64,${ dto.pc_sprite}` : 'images/ui/missignoIcon.png' } 
-            className={  dto.pc_sprite ? 'missigno-icon-defined' : 'missigno-icon'}
-            alt={ dto.name + '.png'} 
-        />
+                <img 
+                src={  dto.pc_sprite ? `data:image/png;base64,${ dto.pc_sprite}` : 'images/ui/missignoIcon.png' } 
+                className={`${styles['missigno-sprite']} 
+                    ${dto.pc_sprite ? styles['missigno-icon-defined'] : styles['missigno-icon']}`}
+
+                alt={ dto.name + '.png'} 
+            />
     );
 }
