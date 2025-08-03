3897a476640b1ef116994e0a25842c1023525583
Miguel Rodríguez <jazmindemar33@gmail.com>
2025-08-03 08:24:55 +0200

    Persist cache & cache config file

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
-import { WeavileTeamBuilderApp } from './WeavileTeamBuilderApp'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+import { globalQueryClient, globalQueryPersister } from './cache';
+import { WeavileTeamBuilderApp } from './WeavileTeamBuilderApp';
 
-import './index.css'
-
-const queryClient = new QueryClient();
+import './index.css';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={ globalQueryClient }
+      persistOptions={ { persister : globalQueryPersister } }>
       <BrowserRouter>
         <WeavileTeamBuilderApp/>
       </BrowserRouter>
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   </StrictMode>,
 )
